1adc1181984d54746c81361c455a78ea33864f83
Shrivu Shankar <shrivu1122@gmail.com>
2018-04-11 18:58:20 -0500

    Added Transactions

---
+++ functions/index.js
@@ -0,0 +1,35 @@
+const functions = require('firebase-functions');
+
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.handleTransaction = functions.database.ref('/transactions/{transID}').onCreate((transaction) => {
+
+  let data = transaction.val();
+  let key = transaction.key;
+
+  admin.database().ref('users/' + data.sender).transaction(function(user) {
+    if (user) {
+      user.balance -= data.amount;
+      if(!user.transactions) {
+        user.transactions = {};
+      }
+      user.transactions[key] = data.timestamp;
+    }
+    return user;
+  });
+
+  admin.database().ref('users/' + data.receiver).transaction(function(user) {
+    if (user) {
+      user.balance += data.amount;
+      if(!user.transactions) {
+        user.transactions = {};
+      }
+      user.transactions[key] = data.timestamp;
+    }
+    return user;
+  });
+
+  return true;
+
+});
--- public/account.js
+++ public/account.js
@@ -3,9 +3,49 @@ class Account {
 
   constructor() {
 
-    this.balanceRef = db.ref(`users/${auth.uid}/balance`);
-    this.balanceRef.on('value', function(snapshot) {
-      $('#cur-balance').html(snapshot.val().toFixed(2) + ' RC');
+    let balanceRef = db.ref(`users/${auth.uid}/balance`);
+    balanceRef.on('value', function(bal) {
+      $('#cur-balance').html(bal.val().toFixed(2) + ' RC');
+    });
+
+    let transactionRef = db.ref(`users/${auth.uid}/transactions`);
+    transactionRef.on('value', function(data) {
+
+      let transactions = data.val();
+
+      if(transactions !== null) {
+        $('#transaction-list').html("");
+      }
+
+      for(let key in transactions) {
+        db.ref(`transactions/${key}`).on('value', function(d) {
+          let transData = d.val();
+          if(transData.sender === auth.uid) {
+            $('#transaction-list').append(`<li class="list-group-item">Purchase <span class="trans-amt badge badge-danger">-${transData.amount}</span></li>`);
+          } else {
+            $('#transaction-list').append(`<li class="list-group-item">Payment <span class="trans-amt badge badge-success">+${transData.amount}</span></li>`);
+          }
+        });
+      }
+
+    });
+
+  }
+
+  sendMoney(toUID, amount) { // account.sendMoney("7vJnUpmeMnaKM8u8E3DbHxGNXfw1", 10)
+
+    let balance = parseFloat($('#cur-balance').html().replace(" RC", ""));
+
+    if(amount < 0 || balance < amount) {
+      return;
+    }
+
+    let newTransactionRef = db.ref('transactions').push();
+    newTransactionRef.set({
+        sender: auth.uid,
+        receiver: toUID,
+        timestamp: firebase.database.ServerValue.TIMESTAMP,
+        amount: amount
     });
 
   }
